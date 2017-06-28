d83d3eca924cf1c6c7018ff3f4ebdf6a73ac26b4
Eli Kimchi <eli.kimchi@pscs.org>
2017-06-28 12:13:15 -0700

    Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Eli and Jack's text adventure, you idiot! Please answer yes/no to yes or no questions and the question number for multiple answer questions.");
+        Scanner consoleInput = new Scanner(System.in);
+        System.out.println("Welcome to Eli and Jack's text adventure, you idiot!\nPlease answer yes/no to yes or no questions and the question number for multiple answer questions. (For instance: Q: Buy a sword, 1. Buy a potion, 2. A: If you want to buy a sword, enter 1.)\nWe'd like to ask you a few questions. First, what's your name? (First name only)");
+        String name = consoleInput.next();
+        System.out.println("Now, how old are you? (In years, you dumbbell)");
+        int age = consoleInput.nextInt();
 
     }
 }
