939190f77e6ece3737b7ab9db5546efa5ccbe983
Eli Kimchi <eli.kimchi@pscs.org>
2017-06-28 12:04:49 -0700

    Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Eli and Jack's text adventure! ");
+        System.out.println("Welcome to Eli and Jack's text adventure, you idiot! Please answer yes/no to yes or no questions and the question number for multiple answer questions.");
+
     }
 }
