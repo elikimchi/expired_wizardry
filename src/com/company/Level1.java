bc99ecdc40c5b898394bd24761f7840c134c595c
jacknote <jack.notarangelo@pscs.org>
2017-06-29 18:21:52 -0700

    Hello

--- src/com/company/Level1.java
+++ src/com/company/Level1.java
@@ -1,9 +1,14 @@
+package com.company;
+
+import edu.digipen.gameobject.GameObject;
+import edu.digipen.gameobject.ObjectManager;
 import edu.digipen.level.GameLevel;
 
 public class Level1 extends GameLevel {
     @Override
     public void create() {
-
+        GameObject obj1 = new GameObject("Object1", 256, 64, "Paddle.png");
+        ObjectManager.addGameObject(obj1);
     }
 
     @Override
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ import edu.digipen.level.EmptyLevel;
 public class Main {
     public static void main(String[] args)
     {
-        Game.initialize(800, 600, 60, new EmptyLevel());
+        Game.initialize(800, 600, 60, new Level1());
     }
 }
